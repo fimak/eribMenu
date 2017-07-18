cd35c062a960d0e75d917e8594ba6e02e3595038
Ufimtsev Aleksandr <AVUfimtsev.SBT@sberbank.ru>
2017-07-18 12:52:27 +0600

    start to implement initialization

--- src/js/script.js
+++ src/js/script.js
@@ -1,73 +1,63 @@
+function serviceTreeBuild (tree) {
+    tree.forEach(function(item, i) {
+        console.log('<li>');
+        console.log('<a>' + item.name + '</a>')
+        if (item.children.length > 0) {
+            console.log('<ul>')
+            serviceTreeBuild(item.children)
+            console.log('</ul>')
+        }
+        console.log('</li>');
+    })
+}
+
 $(document).ready(function() {
-    var table = $('.service-navigation__table')
+    $.ajax({
+        url: 'src/response/init.json',
+        success: function(response) {
+            console.log('init success', response)
+            serviceTreeBuild(response.tree)
+        },
+        error: function() {
+            console.log('init fail')
+        }
+    })
 
-    table.on('click', '.trigger-view', function() {
-        console.log('view')
-        var element = $(this).closest('.service-navigation__item')
-        var id = element.data('id')
-        element.data('view', !element.data('view'))
-        console.log(element.data('view'))
-    });
 
-    table.on('click', '.trigger-remove', function() {
-        console.log('remove')
-        var element = $(this).closest('.service-navigation__item')
-        var id = element.data('id')
 
-        $.ajax({
-            url: 'src/response/remove.json',
-            data: {id: id},
-            success: function() {
-                console.log('remove success')
-                element.remove()
-            },
-            error: function() {
-                console.log('remove fail')
-            }
-        })
-    });
 
-    table.on('click', '.trigger-up', function() {
-        console.log('up')
-        var element = $(this).closest('.service-navigation__item')
-        var id = element.data('id')
 
-        $.ajax({
-            url: 'src/response/up.json',
-            data: {id: id},
-            success: function() {
-                console.log('up success')
-                var prevElement = element.prev()
-                if (prevElement !== 0) {
-                    element.remove()
-                    prevElement.before(element)
-                }
-            },
-            error: function() {
-                console.log('up fail')
-            }
-        })
-    });
 
-    table.on('click', '.trigger-down', function() {
-        console.log('down')
-        var element = $(this).closest('.service-navigation__item')
-        var id = element.data('id')
 
-        $.ajax({
-            url: 'src/response/down.json',
-            data: {id: id},
-            success: function() {
-                console.log('down success')
-                var nextElement = element.next()
-                if (nextElement.length !== 0) {
-                    element.remove()
-                    nextElement.after(element)
-                }
-            },
-            error: function() {
-                console.log('down fail')
-            }
-        })
-    });
+
+
+
+    // var table = $('.service-navigation__table')
+    //
+    // table.on('click', '.trigger-view', function() {
+    //     console.log('view')
+    //     var element = $(this).closest('.service-navigation__item')
+    //     var id = element.data('id')
+    //     element.data('view', !element.data('view'))
+    //     console.log(element.data('view'))
+    // });
+    //
+    // table.on('click', '.trigger-remove', function() {
+    //     console.log('remove')
+    //     var element = $(this).closest('.service-navigation__item')
+    //     var id = element.data('id')
+    //
+    //     $.ajax({
+    //         url: 'src/response/remove.json',
+    //         data: {id: id},
+    //         success: function() {
+    //             console.log('remove success')
+    //             element.remove()
+    //         },
+    //         error: function() {
+    //             console.log('remove fail')
+    //         }
+    //     })
+    // });
+
 });

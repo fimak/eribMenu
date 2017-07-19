70eacabfc6f0e9047858f17a64ffd3ee8d87af3b
Ufimtsev Aleksandr <AVUfimtsev.SBT@sberbank.ru>
2017-07-19 17:07:42 +0600

    service navigation

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,48 @@
-function serviceTreeBuild (tree) {
-    tree.forEach(function(item, i) {
-        console.log('<li>');
-        console.log('<a>' + item.name + '</a>')
-        if (item.children.length > 0) {
-            console.log('<ul>')
-            serviceTreeBuild(item.children)
-            console.log('</ul>')
-        }
-        console.log('</li>');
-    })
+function renderServiceTree (tree) {
+    var html = ''
+
+    function serviceTreeBuild (tree) {
+        tree.forEach(function(item) {
+            if (item.children.length > 0) {
+                html += '<li class="st__item st__item--open">'
+                    + '<a href="#">' + item.name + '</a>'
+                    + '<ul class="st__list">';
+                serviceTreeBuild(item.children);
+                html += '</ul>';
+            } else {
+                html += '<li class="st__item st__item--closed">'
+                    + '<a href="#">' + item.name + '</a>';
+            }
+            html += '</li>';
+        })
+    }
+
+    serviceTreeBuild(tree)
+
+    $('#service-tree-list').html(html);
 }
 
+function renderServiceNavigation(navigation) {
+    var html = ''
+
+    function serviceNavigation(navigation) {
+        navigation.forEach(function(item) {
+
+        })
+    }
+
+    serviceNavigation(navigation)
+    $('#service-tree-list').html(html);
+}
+
+//On Document Ready
 $(document).ready(function() {
     $.ajax({
         url: 'src/response/init.json',
         success: function(response) {
-            console.log('init success', response)
-            serviceTreeBuild(response.tree)
+            console.log('init success', response);
+            renderServiceTree(response.tree)
+            renderServiceNavigation(response.navigation)
         },
         error: function() {
             console.log('init fail')
